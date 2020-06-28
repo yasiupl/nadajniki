9e715bab73e0bc22c9c2096d59d5d2fe7e71fa92
yasiupl <contact@yasiu.pl>
2020-06-28 17:04:42 +0200

    add build date to frontend

--- parse_data.js
+++ parse_data.js
@@ -286,8 +286,6 @@ function processData(json) {
 
     console.log(sources.tags);
 
-    sources.buildTime = Date.now();
-
     saveToFile(JSON.stringify(sources), './src/', 'sources.json');
 
     parseGeoJSON('all', points);
